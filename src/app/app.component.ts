e8d449cca84c3f1e0bcff0229603ba5e24efa24b
tunca.tunc <tunca.tunc@pro-line.com.tr>
2016-11-14 17:35:21 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable'
+import { ChannelService, ConnectionState } from './channel.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit{
+    // An internal "copy" of the connection state stream used because
+    //  we want to map the values of the original stream. If we didn't 
+    //  need to do that then we could use the service's observable 
+    //  right in the template.
+    //   
+    connectionState$: Observable<string>;
+
+    constructor(private channelService: ChannelService) {
+      // Let's wire up to the signalr observables
+      //
+      this.connectionState$ = this.channelService.connectionState$
+        .map((state: ConnectionState) => { return ConnectionState[state]; });
+
+      this.channelService.error$.subscribe(
+          (error: any) => { console.warn(error); },
+          (error: any) => { console.error("errors$ error", error); }
+      );
+
+      // Wire up a handler for the starting$ observable to log the
+      //  success/fail result
+      //
+      this.channelService.starting$.subscribe(
+          () => { console.log("signalr service has been started"); },
+          () => { console.warn("signalr service failed to start!"); }
+      );
+    }
+
+    ngOnInit(){
+      // Start the connection up!
+      console.log('Starting the channel service');
+
+      this.channelService.start();
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,32 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import "rxjs/add/operator/map";
+
+import {ChannelService, ChannelConfig, SignalrWindow} from "./channel.service";
+
 import { AppComponent } from './app.component';
+import { TaskComponent } from './task/task.component';
+
+let channelConfig = new ChannelConfig();  
+channelConfig.url = "http://localhost:9123/signalr";  
+channelConfig.hubName = "EventHub";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [
+    ChannelService, 
+    { provide: SignalrWindow, useValue: window },
+    { provide: 'channel.config', useValue: channelConfig }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
